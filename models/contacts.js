abd711391cb83b69e584e816ca6e0d68d6466ec8
dima-zharyy <zharyydimka@gmail.com>
2022-09-13 22:34:19 +0300

    set deps and added contacts-api

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,63 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const nanoid = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("./contacts.json");
 
-const getContactById = async (contactId) => {}
+const getContacts = async () => {
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+  const parsedContacts = JSON.parse(contacts);
 
-const removeContact = async (contactId) => {}
+  return parsedContacts;
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await getContacts();
+  const contact = contacts.find((item) => item.id === contactId);
 
-const updateContact = async (contactId, body) => {}
+  return contact;
+};
+
+const addContact = async (body) => {
+  const contacts = await getContacts();
+  const id = nanoid();
+  const newContact = { ...body, id };
+  const updatedContacts = [...contacts, newContact];
+  const jsonContacts = JSON.stringify(updatedContacts);
+
+  await fs.writeFile(contactsPath, jsonContacts);
+
+  return newContact;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await getContacts();
+  const contactToRemove = contacts.find((item) => item.id === contactId);
+
+  const updatedContacts = contacts.filter((item) => item.id !== contactId);
+  const jsonContacts = JSON.stringify(updatedContacts);
+
+  await fs.writeFile(contactsPath, jsonContacts);
+
+  return contactToRemove;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await getContacts();
+  const contactToUpdateIndex = contacts.findIndex(
+    (item) => item.id === contactId
+  );
+  if (contactToUpdateIndex < 0) return null;
+
+  contacts.splice(contactToUpdateIndex, 1, { contactId, body });
+
+  return contacts[contactToUpdateIndex];
+};
 
 module.exports = {
-  listContacts,
+  getContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+};
